d8261ac09c0cf100e85c640cbbbce83cd9f176a8
BeatsuDev <ericbieszczad@gmail.com>
2020-03-15 00:36:38 +0100

    Run errors

--- bot/bot.py
+++ bot/bot.py
@@ -6,12 +6,12 @@ from discord.ext import commands
 
 class SpeedDater(commands.Bot):
     def __init__(self, db=None):
-        super().__init__(self, command_prefix=self.get_prefix)
+        super().__init__(command_prefix=self.get_prefix)
 
         self.db = db or dataset.connect('sqlite:///speeddate.db')
 
 
-    def get_prefix(self, bot, message):
+    async def get_prefix(self, bot, message):
         if not isinstance(message.channel, discord.abc.GuildChannel):
             return commands.when_mentioned_or(os.environ.get('prefix', '!'))
 
@@ -22,11 +22,12 @@ class SpeedDater(commands.Bot):
 
 
     async def on_ready(self):
-        print(  f"Connected to {len(self.bot.users)} users",
-                f"from {len(self.bot.guilds)} guilds\n\n------------")
+        print(  f"Connected to {len(self.users)} users",
+                f"from {len(self.guilds)} guilds\n\n------------")
 
         print('Loading cogs')
         for file in os.listdir( os.path.join(os.getcwd(),'bot','cogs') ):
             if file.endswith('.py'):
-                print(f'Loading: bot.cogs.{file}')
-                self.load_extension(f'bot.cogs.{file}')
+                print(f'Loading: bot.cogs.{file[:-3]}')
+                self.load_extension(f'bot.cogs.{file[:-3]}')
+                print(f'Loaded!')
